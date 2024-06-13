4ebd3bf9e912f707e9a1ac3d6d349efa43fe1de9
David Mattei <david.mattei@sappico.be>
2024-06-13 21:48:22 +0200

    feat: slim app

--- app/symfony/src/Kernel.php
+++ app/symfony/src/Kernel.php
@@ -1,11 +1,45 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App;
 
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Bundle\MakerBundle\MakerBundle;
+use Symfony\Bundle\TwigBundle\TwigBundle;
+use Symfony\Bundle\WebProfilerBundle\WebProfilerBundle;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    protected function configureContainer(ContainerConfigurator $container): void
+    {
+        $configDir = $this->getConfigDir();
+
+        $container->import($configDir.'/{packages}/*.yaml');
+        $container->import($configDir.'/{packages}/'.$this->environment.'/*.yaml');
+
+        $container->services()
+            ->load('App\\', __DIR__.'/*')
+            ->exclude('../src/{DependencyInjection,Entity,Kernel.php}')
+            ->autowire()
+            ->autoconfigure()
+        ;
+    }
+
+    protected function configureRoutes(RoutingConfigurator $routes): void
+    {
+        if ('dev' === $this->getEnvironment()) {
+            $routes->import('@WebProfilerBundle/Resources/config/routing/wdt.xml')->prefix('/_wdt');
+            $routes->import('@WebProfilerBundle/Resources/config/routing/profiler.xml')->prefix('/_profiler');
+            $routes->import('@FrameworkBundle/Resources/config/routing/errors.xml')->prefix('/_error');
+        }
+
+        $routes->import(__DIR__.'/Controller/', 'annotation');
+    }
 }
